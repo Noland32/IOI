3b869f4a11573ff23afd432029bb0875715f58b0
Noland32 <aymard.ph@gmail.com>
2018-07-29 16:37:52 +0200

    création IOI

---
+++ src/classCours.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class classCours 	
+{
+	public static void main(String[] args)
+	{
+		Scanner entrée = new Scanner(System.in);
+		int nbEmplacements = entrée.nextInt();
+		int[] marchands = new int[nbEmplacements];
+		for (int marchand =0; marchand <5; marchand++) {
+			marchands[marchand] = entrée.nextInt();
+		}
+  }
+}
